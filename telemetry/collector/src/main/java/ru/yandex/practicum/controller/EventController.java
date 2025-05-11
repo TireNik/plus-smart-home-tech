a801fc33c57b6d30221066c7e7f89359c1ec2df3
TireNik <sugarman.sviridov@yandex.ru>
2025-05-11 20:17:28 +0300

    fix application.yml

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/EventController.java
@@ -1,38 +1,30 @@
 package ru.yandex.practicum.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import ru.yandex.practicum.service.EventService;
 import ru.yandex.practicum.model.HubEvent;
 import ru.yandex.practicum.model.SensorEvent;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
 
-    private final Map<String, EventService> eventServices;
+    private final EventService service;
 
-    public EventController(Map<String, EventService> eventServices) {
-        this.eventServices = eventServices;
+    @PostMapping("/hubs")
+    public void sendHubEvent(@Valid @RequestBody HubEvent ev) {
+        service.processEvent(ev);
     }
 
     @PostMapping("/sensors")
-    public ResponseEntity<Void> collectSensorEvent(@Valid @RequestBody SensorEvent event) {
-        EventService service = eventServices.get("sensorEventService");
-        service.processEvent(event);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/hubs")
-    public ResponseEntity<Void> collectHubEvent(@Valid @RequestBody HubEvent event) {
-        EventService service = eventServices.get("hubEventService");
-        service.processEvent(event);
-        return ResponseEntity.ok().build();
+    public void sendSensorEvent(@Valid @RequestBody SensorEvent ev) {
+        service.processEvent(ev);
     }
 }
