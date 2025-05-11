601472606236c1482fec58647e501c75e1facad3
TireNik <sugarman.sviridov@yandex.ru>
2025-05-11 15:55:25 +0300

    fix pom

--- telemetry/collector/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
