d743a974db26cffde4ebde7b448815d81e4a5dbb
TireNik <sugarman.sviridov@yandex.ru>
2025-05-13 17:52:59 +0300

    Внес правки

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventService.java
@@ -1,28 +1,28 @@
 package ru.yandex.practicum.service;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import ru.yandex.practicum.config.KafkaConfig;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.mappers.SensorEventMapper;
 import ru.yandex.practicum.model.SensorEvent;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service("sensorEventService")
 @RequiredArgsConstructor
 public class SensorEventService implements EventService {
-
-    private static final String TOPIC = "telemetry.sensors.v1";
-
-    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
+    final KafkaConfig kafkaConfig;
 
     @Override
     public void processEvent(Object event) {
         if (event instanceof SensorEvent sensorEvent) {
             SensorEventAvro avro = SensorEventMapper.toAvro(sensorEvent);
-            kafkaTemplate.send(TOPIC, sensorEvent.getId(), avro);
+            kafkaConfig.getProducer().send(
+                    new ProducerRecord<>(kafkaConfig.getSensorEventsTopic(), sensorEvent.getHubId(), avro)
+            );
         } else {
             throw new IllegalArgumentException("Неверный тип события: ожидается SensorEvent");
         }
     }
+
 }
