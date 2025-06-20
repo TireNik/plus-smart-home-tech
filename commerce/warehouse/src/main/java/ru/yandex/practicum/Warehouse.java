5fe75b633d572bb87b0d41ccb3e83c447906f130
TireNik <sugarman.sviridov@yandex.ru>
2025-06-20 16:13:47 +0300

    fix warehouse

--- commerce/warehouse/src/main/java/ru/yandex/practicum/Warehouse.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Warehouse.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
@@ -7,6 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class Warehouse {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+             SpringApplication.run(Warehouse.class, args);
     }
 }
